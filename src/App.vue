ebf0e7056b78e32cb644e0f8e02b39a86491d666
kl423 <79741134+kl423@users.noreply.github.com>
2022-03-14 23:09:10 +0800

    created opportunities tab, added search bars and methods to retrieve values

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Opportunities/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Opportunities from './components/Opportunities.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Opportunities
   }
 }
 </script>
