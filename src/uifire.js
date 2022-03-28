1e4b6ff5fdbebc2039655e3b6d245a25542da6bd
Hanyu0118 <77627280+Hanyu0118@users.noreply.github.com>
2022-03-28 12:20:51 +0800

    OrganisationAccPage links to fb edit1

--- src/uifire.js
+++ src/uifire.js
@@ -1,20 +1,12 @@
 import firebase from 'firebase/compat/app'
 
-/*const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDUwI_RNBITayvz2cb-JvQAiy1f0YbFLno",
     authDomain: "bt3103-f6b2e.firebaseapp.com",
     projectId: "bt3103-f6b2e",
     storageBucket: "bt3103-f6b2e.appspot.com",
     messagingSenderId: "588147986857",
     appId: "1:588147986857:web:66f43af419da6e2b6474b8"
-  };*/
-  const firebaseConfig = {
-    apiKey: "AIzaSyBFey_PtGKxSJhpspHGQlyPFKv-6m5frs8",
-    authDomain: "bt3103demo-16937.firebaseapp.com",
-    projectId: "bt3103demo-16937",
-    storageBucket: "bt3103demo-16937.appspot.com",
-    messagingSenderId: "293423883020",
-    appId: "1:293423883020:web:1c8593e35965a60352df29"
   };
   
 
