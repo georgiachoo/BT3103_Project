9e7d9bc4fcdbcf8b0fd3f43c53e125662c0a0f8f
Hanyu0118 <77627280+Hanyu0118@users.noreply.github.com>
2022-04-01 11:10:00 +0800

    add view other users

--- src/components/SelectVolunteer.vue
+++ src/components/SelectVolunteer.vue
@@ -11,18 +11,52 @@
         <th>Option</th>
         <th>Manage</th>
 
+
         
         </tr>
         </thead>
     </table> <br><br>
     </div>
 
-    <!-- code for modal + registration button -->
+ 
     
 
     <div id = "search_btn">
-            <button v-on:click = "searchQuery()" style = "margin: 45px 10px 25px;">View Other users</button>
-        </div>
+            <button v-on:click = "displayModalOtherUsers()" style = "margin: 45px 10px 25px;">View Other users</button>
+    </div>
+
+    <div id = "eventModal" class = "modal">
+         <div class = "modal-content">
+             <span class = "closeBtn" >&times;</span>
+             <h2> {{ name }} </h2>
+
+             <p> <strong>Email:</strong> {{ email }} </p>
+             <p> <strong>Age:</strong> {{ age }} </p>
+             <p> <strong>Gender:</strong> {{ gender }} </p>
+             <p> <strong>Certifications:</strong> {{ certifi }} </p>
+             <p> <strong>Experience:</strong> {{ exper }} </p>
+             <p> <strong>Interests:</strong> {{ interests }} </p>
+             <p> <strong>Skills:</strong> {{ skills }} </p>
+             <p> <strong>Introduction:</strong> {{ intro }} </p>
+             <button  style = "margin: 45px 10px 25px;">Message</button>
+
+         </div>
+
+     </div>
+
+     <div id = "eventModalOtherUsers" class = "modalOtherUsers">
+         <div class = "modal-contentOtherUsers">
+             <span class = "closeBtnOtherUsers" >&times;</span>
+             <h1>Users Profiles</h1>
+             <table id= "otherUsersTable" class = "auto-index">
+        <thead>
+        </thead>
+    </table>
+
+         </div>
+
+     </div>
+
     
 
 </template>
@@ -53,7 +87,17 @@ export default {
           eventDL: 'deadLine',
           eventNumV: 'numVol',
           eventOrg: 'organisation',
-          orgName:"organisationName"
+          orgName:"organisationName",
+          age: 'a',
+          certifi: 'c',
+          userEmail: 'u',
+          exper: 'e',
+          gender: 'g',
+          interests: 'i',
+          intro: 'i',
+          name: 'n',
+          skills: 's',
+          oldCustomers: new Array(),
       }
     },
 
@@ -70,7 +114,8 @@ export default {
         this.displayAll(); 
 
         // add event listerner to close button in modal
-        //document.getElementsByClassName('closeBtn')[0].addEventListener('click', this.closeModal);
+        document.getElementsByClassName('closeBtn')[0].addEventListener('click', this.closeModal);
+        document.getElementsByClassName('closeBtnOtherUsers')[0].addEventListener('click', this.closeModalOtherUsers);
 
     },
 
@@ -97,21 +142,31 @@ export default {
         async getUserNameByEmailAndDisplayEachRow(email,ind,eventName) {
             var thisInstance = this; 
             var nameRef = await getDoc(doc(db,"Users",email));
-            var name = nameRef.data().Name
+            var name = nameRef.data().Name;
+            var age = nameRef.data().Age;
+            var certifi = nameRef.data().Certifications;
+            var exper = nameRef.data().Experience;
+            var gender = nameRef.data().Gender;
+            var interests = nameRef.data().Interests;
+            var intro = nameRef.data().Introduction;
+            var skills = nameRef.data().Skills;
             var table = document.getElementById("table");
             var row = table.insertRow(ind);
             var cell1 = row.insertCell(0);
             cell1.innerHTML = name + " signed up for " + eventName; 
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
+            
              
             var bu = document.createElement("button");
             bu.className = "bwt";
             //bu.id = String(eName);
             bu.innerHTML = "View Profile";
             bu.onclick = function() {
-                //thisInstance.displayModal(eName, eDesc, eCat, eLoc, eDate, eSkills, eDL, eNumV, eOrg,eOrgName);
+
+                thisInstance.displayModal(age,certifi,email,exper,gender,interests,intro,name,skills);
             }
+            
             cell2.appendChild(bu); 
             var bu2 = document.createElement("button");
             bu2.className = "bwt";
@@ -125,6 +180,10 @@ export default {
                 thisInstance.displayAll()
             }
             cell3.appendChild(bu2); 
+            this.oldCustomers.push(email)
+            console.log(this.oldCustomers)
+
+            
             return
 
         },
@@ -149,14 +208,6 @@ export default {
             while (table.rows.length > 1) {
                 table.deleteRow(1);
             }
-
-            //var thisInstance = this; 
-            
-            //var auth = getAuth();
-            //var currUser = auth.currentUser.email;
-            //this.user = currUser;
-            //console.log(this.user)
-
             let ind = 1;
 
             result.forEach((docs) => {
@@ -172,53 +223,126 @@ export default {
                     for (let i = 0;i < arrayLen;i++) {
                         console.log(y.Applied_volunteers[i])
                         this.getUserNameByEmailAndDisplayEachRow(y.Applied_volunteers[i],ind,y.Event_Name);
-                        
-                        
-                        
-                    }
+                        }
                     ind += 1;
 
                 }
-                /*var row = table.insertRow(ind);
-
-                var eName = (y.Event_Name);
-                console.log(y.Date)
-                var eDate = ((y.Date).toDate()).toDateString();
-                var eLoc = (y.Location);
-                var eCat = (y.Category);
-                var eOrgName = (y.Organisation_Name);
-                var eSkills = (y.Required_skills);
-                
-
-
-                // variables to be displayed when user clicks on view (to be passed to displayModal)
-                var eDesc = (y.Description);
-                var eDL = ((y.Deadline_of_sign_up).toDate()).toDateString();
-                var eNumV = (y.Number_of_volunteers_needed);
-                var eOrg = (y.Org_Email);
-
-
-                var cell1 = row.insertCell(0); var cell2 = row.insertCell(1); var cell3 = row.insertCell(2);
-                var cell4 = row.insertCell(3); var cell5 = row.insertCell(4); var cell6 = row.insertCell(5);
-
-                cell1.innerHTML = eName; cell2.innerHTML = eCat; cell3.innerHTML = eLoc; 
-                cell4.innerHTML = eDate; cell5.innerHTML = eOrgName;
-
-                var bu = document.createElement("button");
-                bu.className = "bwt";
-                bu.id = String(eName);
-
-                bu.innerHTML = "View";
-                bu.onclick = function() {
-                    thisInstance.displayModal(eName, eDesc, eCat, eLoc, eDate, eSkills, eDL, eNumV, eOrg,eOrgName);
-                }
-                cell6.appendChild(bu);
-                */
-
+               
                 
             });
             return
         },
+
+        changeValue(age,certifi,email,exper,gender,interests,intro,name,skills) {
+            this.name = name;
+            this.age = age;
+            this.certifi = certifi;
+            this.exper = exper;
+            this.gender = gender;
+            this.interests = interests;
+            this.intro = intro;
+            this.skills = skills;
+            this.email = email;
+         },
+         displayModal(age,certifi,email,exper,gender,interests,intro,name,skills) {
+             // get event info as arguments from displayTable (or query)
+
+             // update mustache values
+             this.changeValue(age,certifi,email,exper,gender,interests,intro,name,skills);
+
+             console.log("display")
+             // make modal visible
+             this.openModal();
+         },
+         async displayModalOtherUsers() {
+             // get event info as arguments from displayTable (or query)
+
+             // update mustache values
+             //this.changeValue();
+             const allUsers = await getDocs(collection(db,"Users"));
+             var table = document.getElementById("otherUsersTable");
+            while (table.rows.length > 0) {
+                table.deleteRow(0);
+            }
+            let ind = 0;
+           
+
+            allUsers.forEach((docs) => {
+                var y = docs.data();
+                var name = y.Name;
+                var intro = y.Introduction;
+                var email = docs.id;
+                if(!this.oldCustomers.includes(email)) {
+                    this.otherUsersRow(name,intro,ind);
+                    ind += 1;
+                }
+               
+                
+            });
+
+            
+
+             
+
+             console.log("display")
+             // make modal visible
+             this.openModalOtherUsers();
+         },
+
+         async otherUsersRow(name,intro,ind) {
+            //var thisInstance = this; 
+            var table = document.getElementById("otherUsersTable");
+            var row = table.insertRow(ind);
+            var cell1 = row.insertCell(0);
+            //cell1.innerHTML = name + "----" + intro;
+            var twoLines = document.createElement("table")
+            var nameLine = twoLines.insertRow(0);
+            var nameCell = nameLine.insertCell(0);
+            nameCell.innerHTML = name;
+            nameCell.style.font = "bold 18px arial,serif"
+            nameCell.style.textAlign = "left"
+            var introLine = twoLines.insertRow(1);
+            var introCell = introLine.insertCell(0);
+            introCell.innerHTML = intro;
+            cell1.appendChild(twoLines)
+            var cell2 = row.insertCell(1);
+            
+             
+            var bu = document.createElement("button");
+            bu.className = "bwt";
+            //bu.id = String(eName);
+            bu.innerHTML = "Message";
+            bu.onclick = function() {
+
+                //thisInstance.displayModal(age,certifi,email,exper,gender,interests,intro,name,skills);
+            }
+            cell2.appendChild(bu); 
+
+         },
+
+
+         openModal() {
+             var modal = document.getElementById('eventModal');
+             modal.style.display = "block";
+         },
+
+
+         closeModal() {
+             var modal = document.getElementById('eventModal');
+             modal.style.display = 'none';
+         },
+
+         openModalOtherUsers() {
+             var modal = document.getElementById('eventModalOtherUsers');
+             modal.style.display = "block";
+         },
+
+
+         closeModalOtherUsers() {
+             var modal = document.getElementById('eventModalOtherUsers');
+             modal.style.display = 'none';
+         },
+
 
 
 
@@ -314,5 +438,38 @@ export default {
         text-decoration: none;
         cursor: pointer;
     }
+
+    .modalOtherUsers {
+        display: none;
+        position: fixed;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        overflow: auto;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .modal-contentOtherUsers {
+        background-color: #f4f4f4;
+        margin: 20% auto;
+        padding: 10px;
+        width: 90%;
+    }
+
+    .closeBtnOtherUsers {
+        color: #ccc;
+        float: right;
+        font-size: 30px;
+    }
+
+    .closeBtnOtherUsers:hover,.closeBtnOtherUsers:focus{
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    
 
 </style>
