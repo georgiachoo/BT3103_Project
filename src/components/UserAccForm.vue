aaefaf534b2bf14c5ec475a8ed403ba3cc43d36e
Georgia Choo <georgiachooo@gmail.com>
2022-04-01 20:41:20 +0800

    improved user acc & notif
    combined saving profile pic code, connect user notif and messages, connected user notif to firebase

--- src/components/UserAccForm.vue
+++ src/components/UserAccForm.vue
@@ -1,6 +1,5 @@
 <template>
-   <div> <!-- v-if="user" -->
-    <!-- <img id="profilePic"> -->
+   <div>
     <img id="pic" v-bind:src=profilePic>
     <input type="file" id="input" accept="image/*">
     <label for="input" id="uploadBtn"> Choose Profile Picture</label><br><br>
@@ -8,14 +7,11 @@
   
   <div>
     <img id="gamePic" :src="game">
-    <label for="gamePic" id="score"> Score: </label><br><br>
+    <label for="gamePic" id="score"> Score: {{score}}</label><br><br>
   </div>
 
-  <br>
-  <br>
-
   <form id="accForm">
-    <label for="name" > Name: </label>
+    <label for="name"> Name: </label>
     <input type="text" id="name" required="" placeholder="Enter your name"> <br><br>
 
     <label for="age"> Age: </label>
@@ -23,9 +19,6 @@
 
     <label for="gender"> Gender: </label>
     <input type="text" id="gender" required="" placeholder="Male/Female/Non-binary"> <br><br>
-
-    <label for="email"> Email: </label>
-    <input type="email" id="email" required="" placeholder="Enter your email"><br><br>
 
     <label for="skills"> Skills: </label>
     <textarea name="skills" id="skills" cols="30" rows="5" placeholder="Relevant skills"></textarea><br><br>
@@ -48,7 +41,6 @@
   </form>
 
   <div id="allInfo">
-    <!-- <img id="pic" v-bind:src=profilePic> -->
     <p> Name: {{name}} </p>
     <p> Age: {{age}} </p>
     <p> Gender: {{gender}} </p>
@@ -66,8 +58,8 @@
 import firebaseApp from '../firebase.js';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
-import { doc, updateDoc, getDoc, setDoc } from 'firebase/firestore';
-import { getStorage, ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, getDownloadURL, uploadString } from 'firebase/storage';
 
 const db = getFirestore(firebaseApp);
 
@@ -86,20 +78,50 @@ export default {
         cert: "",
         exp: "",
         intro: "",
+        score: 0,
         game: require('../assets/game.png'),
-        profilePic: "",
+        profilePic: ""
       }
-    },
-
-    beforeMount() {
-      this.loadData();
     },
 
     mounted() {
       const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
+      onAuthStateChanged(auth, async (user) => {
         if (user) {
           this.user = user;
+          this.email = user.email;
+      
+          const currDoc = doc(db, "Users", this.email);
+          const docRef = await getDoc(currDoc);
+
+          if (docRef.exists()) {
+            const accForm = document.getElementById("accForm");
+            accForm.style.display = "none";
+            
+            this.name = docRef.data().Name;
+            this.age = docRef.data().Age;
+            this.gender = docRef.data().Gender;
+            this.skills = docRef.data().Skills;
+            this.interests = docRef.data().Interests; 
+            this.cert = docRef.data().Certifications;
+            this.exp = docRef.data().Experience;
+            this.intro = docRef.data().Introduction;
+            this.score = docRef.data().Score;
+
+            const docdata = docRef.data();
+            this.profilePic = docdata['profilePic'];
+
+            const infoContainer = document.getElementById("allInfo");
+            infoContainer.style.display = "block";
+            document.getElementById("uploadBtn").style.display = "none";
+
+          } else {
+            await setDoc(doc(db, "Users", this.email), {
+              Email: this.email, 
+              Score: 0, 
+              profilePic: "@/assets/volunteer.png"
+            });
+          }
         }
       });
 
@@ -110,98 +132,19 @@ export default {
         var fileReader = new FileReader();
         var fileObject = this.files[0];
         fileReader.readAsDataURL(fileObject);
-        console.log("fileObject:")
-        console.log(fileObject)
         fileReader.onload = () => {
           var result = fileReader.result;
           var img = document.querySelector('#pic');
           img.setAttribute('src', result); 
-
         };
       }
 
-      this.loadData();
-    },
-
-    methods: {
-      async loadData() {
-
-        const auth = getAuth();
-        var email = auth.currentUser.email;
-        
-        const docRef = doc(db, "Users", email);
-        const docSnap = await getDoc(docRef);
-        const docdata = docSnap.data();
-
-        if (docdata != undefined) {
-
-          this.name = docdata['Name'] !== undefined ? docdata['Name'] : "",
-          this.age = docdata['Age'] !== undefined ? docdata['Age'] : "",
-          this.gender = docdata['Gender'] !== undefined ? docdata['Gender'] : "",
-          this.email = docdata['Email'] !== undefined ? docdata['Email'] : email,
-          this.skills = docdata['Skills'] !== undefined ? docdata['Skills'] : "",
-          this.interests = docdata['Interests'] !== undefined ? docdata['Interests'] : "",
-          this.cert = docdata['Certifications'] !== undefined ? docdata['Certifications'] : "",
-          this.exp = docdata['Experience'] !== undefined ? docdata['Experience'] : "",
-          this.intro = docdata['Introduction'] !== undefined ? docdata['Introduction'] : "",
-          this.profilePic = docdata['profilePic'] !== undefined ? docdata['profilePic'] : "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-          console.log(docSnap.data())
-
-          console.log(this.name)
-          console.log(this.age)
-          console.log(this.gender)
-          console.log(this.email)
-          console.log(this.skills)
-          console.log(this.interests)
-          console.log(this.cert)
-          console.log(this.exp)
-          console.log(this.intro)
-          console.log(this.profilePic)
-        } else {
-
-            await setDoc(doc(db, "Users", email), {
-              Name: "",
-              Age: "",
-              Gender: "",
-              Email: email,
-              Skills: "",
-              Interests: "",
-              Certifications: "",
-              Experience: "",
-              Introduction: "",
-              profilePic: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-            });
-
-            this.email = email
-            this.profilePic = "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
-        }
-      },
-      async savetofs() {
-        const auth = getAuth();
-        email = auth.currentUser.email;
-        // this.user = auth.currentUser.email;
-
-        var a = document.getElementById("name").value 
-        var b = document.getElementById("age").value 
-        var c = document.getElementById("gender").value 
-        var d = document.getElementById("email").value
-        var e = document.getElementById("skills").value
-        var f = document.getElementById("interest").value
-        var g = document.getElementById("cert").value
-        var h = document.getElementById("exp").value
-        var i = document.getElementById("intro").value
-        //var j = document.getElementById("input").value
-        alert("Saving changes to My Account");
-
-        const file = document.getElementById("pic").src
+      const file = document.getElementById("pic").src
         if (file.substring(0,4) !== 'http' && file !== '') {
-
           const fileURL = file.split(',')[1]
-
           const metadata = {
             contentType: 'image/jpeg',
           };
-
           const storage = getStorage();
           const auth = getAuth();
           var email = auth.currentUser.email
@@ -211,24 +154,54 @@ export default {
             getDownloadURL(snapshot.ref).then((downloadURL) => {
               console.log('File available at', downloadURL);
               const userRef = doc(db, "Users", email);
-              updateDoc(userRef, {
-                profilePic: downloadURL
-              });
-          })});
+              updateDoc(userRef, {profilePic: downloadURL});
+            })
+          });
+        }
+    },
+
+    methods: {
+      async savetofs() {
+
+        var a = document.getElementById("name").value 
+        var b = document.getElementById("age").value 
+        var c = document.getElementById("gender").value 
+        var e = document.getElementById("skills").value
+        var f = document.getElementById("interest").value
+        var g = document.getElementById("cert").value
+        var h = document.getElementById("exp").value
+        var i = document.getElementById("intro").value
+
+        alert("Saving changes to My Account");
+
+        const file = document.getElementById("pic").src
+        if (file.substring(0,4) !== 'http' && file !== '') {
+          const fileURL = file.split(',')[1]
+          const metadata = {
+            contentType: 'image/jpeg',
+          };
+          const storage = getStorage();
+          const auth = getAuth();
+          var email = auth.currentUser.email
+          var imgName = email + '.jpg'
+          const storageRef = ref(storage, imgName);
+          uploadString(storageRef, fileURL, 'base64', metadata).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((downloadURL) => {
+              console.log('File available at', downloadURL);
+              const userRef = doc(db, "Users", email);
+              updateDoc(userRef, {profilePic: downloadURL});
+            })
+          });
         }
 
-        
         const accForm = document.getElementById("accForm");
         accForm.style.display = "none";
 
         try {
-
-          const docRef = await updateDoc(doc(db, "Users", email), 
-            {Name: a, Age: b, Gender: c, Email: d, Skills: e, Interests: f, Certifications: g, 
+          const docRef = await updateDoc(doc(db, "Users", this.email), 
+            {Name: a, Age: b, Gender: c, Skills: e, Interests: f, Certifications: g, 
             Experience: h, Introduction: i});
           console.log(docRef);
-          //this.$emit('userinfo');
-
         }
         catch(error) {
           console.error("Error making changes: ", error);
@@ -237,19 +210,16 @@ export default {
         this.name = a;
         this.age = b;
         this.gender = c;
-        this.email = d;
         this.skills = e; 
         this.interests = f; 
         this.cert = g; 
         this.exp = h; 
         this.intro = i;
 
-        const docRef = doc(db, "Users", email);
+        const docRef = doc(db, "Users", this.email);
         const docSnap = await getDoc(docRef);
         const docdata = docSnap.data();
         this.profilePic = docdata['profilePic'];
-        console.log("test")
-        console.log(this.profilePic)
 
         const infoContainer = document.getElementById("allInfo");
         infoContainer.style.display = "block";
@@ -263,7 +233,6 @@ export default {
         infoContainer.style.display = "none";
         const uploadBtn = document.getElementById("uploadBtn");
         uploadBtn.style.display = "inline-block";
-
       }
     }
 }
@@ -289,8 +258,7 @@ export default {
   color: black;
   line-height: 20px;
   font-size: 15px;
-  transform: translate(-100%, 95%);
-  display: none;
+  transform: translate(-100%, 95%)
 }
 #gamePic{
   height: 70px;
@@ -299,7 +267,7 @@ export default {
   transform: translate(-530%, -150%);
 }
 #score{
-  transform: translate(90%, -150%);
+  transform: translate(100%, -100%);
 }
 label {
   display: inline-block;
@@ -318,7 +286,7 @@ body{
   text-align: center;
 }
 #allInfo{
-  display: block;
+  display: none;
 }
 #editBtn{
   background: rgba(231, 207, 27, 0.904);
@@ -326,7 +294,7 @@ body{
   cursor: pointer;
   text-align: center;
   padding: 7px 14px;
-  border: block;
+  border: none;
 }
 #savebutton{
   background: rgba(231, 207, 27, 0.904);
@@ -335,10 +303,5 @@ body{
   text-align: center;
   padding: 7px 14px;
   border: none;
-}
-
-#accForm{
-  display: none;
-  text-align: center;
 }
 </style>
