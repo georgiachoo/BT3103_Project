4ce2dfb56f65ea244a798a954023036db65c54b9
shavontan <shavontanqh@gmail.com>
2022-04-08 14:14:51 +0800

    updated chat feature to display name and profile pic of user

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -13,8 +13,11 @@
 </template>
 
 <script>
+import firebaseApp from '../firebase.js';
 import Talk from "talkjs";
 import { getAuth } from "firebase/auth"
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+const db = getFirestore(firebaseApp);
 
     export default {
         name: "Messages",
@@ -38,20 +41,48 @@ import { getAuth } from "firebase/auth"
 
           // to query name and photo from firebase for 'me' and 'other' user
           var email = this.email
-          console.log(this.email)
+          const userRef = await getDoc(doc(db, "Users", this.email))
+          const userData = userRef.data();
+          console.log(userData)
+
 
           var otherid = this.$route.params.otherID;
           if (otherid == null) {
               otherid = ''
           }
-        
+
+          const otherRef = await getDoc(doc(db, "Users", otherid))
+          const otherData = otherRef.data()
+          console.log(otherData)
+
+
+          var othername = otherid
+          var myname = this.email
+          var otherpic = "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg"
+          var mypic = "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg"
+
+          if (userData != undefined) {
+            myname = userData['Name']
+            mypic = userData['profilePic']
+            
+          } 
+
+          if (otherData != undefined) {
+            othername = otherData['Name']
+            otherpic = otherData['profilePic']
+
+          }
+  
+          
+
+
           Talk.ready.then(function() {
 
             var me = new Talk.User({
               id: email,
-              name: email,
+              name: myname,
               email: email,
-              photoUrl: "https://thumbs.dreamstime.com/b/man-profile-cartoon-smiling-round-icon-vector-illustration-graphic-design-135443422.jpg",
+              photoUrl: mypic,
               role: "default",
             });
 
@@ -65,7 +96,8 @@ import { getAuth } from "firebase/auth"
             if (otherid !== '') {
               var other = new Talk.User({
                 id: otherid,
-                name: otherid,
+                name: othername,
+                photoUrl: otherpic,
                 role: "default",
 
               });
